85aa7f18a964cd1d46a0c7d3f6816b4d71d629f7
not-nl <not-nl@users.noreply.github.com>
2023-11-23 05:03:41 -0800

    backend: Add /register endpoint

---
+++ backend/app/auth/__init__.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, jsonify, make_response, abort
+from flask_jwt_extended import create_access_token, JWTManager
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_apispec import doc, use_kwargs, marshal_with
+from webargs import fields
+from sqlalchemy import exc
+
+from app.database import Session
+from app.api.v1.user.service import (
+    get as get_user,
+    create as create_user,
+)
+from app.api.v1 import docs
+
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+jwt = JWTManager()
+
+# Register a callback function that takes whatever object is passed in as the
+# identity when creating JWTs and converts it to a JSON serializable format.
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.username
+
+# Register a callback function that loads a user from your database whenever
+# a protected route is accessed. This should return any python object on a
+# successful lookup, or None if the lookup failed for any reason (for example
+# if the user has been deleted from the database).
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return get_user(Session, identity)
+
+
+@doc(
+    summary='Register',
+    description='Create a new user with the passed username and password.',
+    tags=['auth']
+)
+@bp.route('/register', methods=['POST'])
+@use_kwargs({
+    'username': fields.Str(required=True),
+    'password': fields.Str(required=True),
+})
+@marshal_with(None, code=200)
+def register(username, password):
+    try:
+        create_user(Session, username, generate_password_hash(password))
+    except exc.IntegrityError as e:
+        abort(400, description='Username is already used')
+    return make_response('', 200)
+docs.register(register, blueprint='auth')
