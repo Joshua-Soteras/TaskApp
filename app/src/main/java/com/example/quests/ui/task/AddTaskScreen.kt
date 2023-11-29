b9bd364aa757e5b5361dc32c5b3c8e56dcb5ad51
not-nl <not-nl@users.noreply.github.com>
2023-11-29 02:04:17 -0800

    ui: Set up DatePicker on add task screen
    The add task view model is a WIP. See comments in AddTaskViewModel.kt
    for the planned logic.

--- app/src/main/java/com/example/quests/ui/task/AddTaskScreen.kt
+++ app/src/main/java/com/example/quests/ui/task/AddTaskScreen.kt
@@ -1,6 +1,8 @@
 package com.example.quests.ui.task
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,14 +12,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -29,14 +38,17 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.quests.R
 import com.example.quests.ui.navigation.NavigationDestination
 import com.example.quests.ui.util.AddTaskTopAppBar
+import com.example.quests.ui.util.toString
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import java.util.Date
 
 object AddTaskDestination : NavigationDestination {
     override val route = "add_task"
     override val titleRes = R.string.create_task
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddTaskScreen(
     navigateBack: () -> Unit,
@@ -44,6 +56,9 @@ fun AddTaskScreen(
     coroutineScope: CoroutineScope = rememberCoroutineScope(),
     viewModel: AddTaskViewModel = hiltViewModel()
 ) {
+    val openDateDialog = remember { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState()
+
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = {
@@ -66,8 +81,49 @@ fun AddTaskScreen(
                     navigateBack()
                 }
             },
+            date = datePickerState.selectedDateMillis?.let {
+                Date(it).toString("EEE, MMM d, yyyy")
+            },
+            onDatePickerClick = { openDateDialog.value = true },
             modifier = Modifier.padding(paddingValues)
         )
+
+        if (openDateDialog.value) {
+            DatePickerDialog(
+                onDismissRequest = { openDateDialog.value = false },
+                // Dumb hack, the dialog does not resize to fit other composables so we're
+                // squeezing all the buttons into one row
+                confirmButton = {
+                    Row {
+                        TextButton(onClick = { datePickerState.setSelection(null) }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.block_24px),
+                                contentDescription = null
+                            )
+                            Spacer(modifier = Modifier.padding(horizontal = 4.dp))
+                            Text(stringResource(R.string.no_date))
+                        }
+                        Spacer(Modifier.weight(1f))
+                        TextButton(onClick = { openDateDialog.value = false }) {
+                            Text(stringResource(R.string.cancel))
+                        }
+                        TextButton(
+                            onClick = {
+                                openDateDialog.value = false
+                                viewModel.updateSelectedDate(datePickerState.selectedDateMillis)
+                                println(datePickerState.selectedDateMillis)
+                            },
+                        ) {
+                            Text(stringResource(R.string.ok))
+                        }
+                    }
+                }
+            ) {
+                DatePicker(
+                    state = datePickerState
+                )
+            }
+        }
     }
 }
 
@@ -79,6 +135,8 @@ private fun AddTaskContent(
     onTitleChange: (String) -> Unit,
     onDescriptionChange: (String) -> Unit,
     onSaveClick: () -> Unit,
+    date: String?,
+    onDatePickerClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -124,6 +182,8 @@ private fun AddTaskContent(
                 disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
             ),
         )
+        // Dumb hack because Compose still gives space to label even if label is null
+        DatePickerTextField(date, onDatePickerClick)
         Spacer(modifier = Modifier.size(32.dp)) // TODO: extract this as dimen res
         Button(
             onClick = onSaveClick,
@@ -133,5 +193,62 @@ private fun AddTaskContent(
         ) {
             Text(text = stringResource(R.string.save))
         }
+    }
+}
+
+@Composable
+private fun DatePickerTextField(
+    date: String?,
+    onDatePickerClick: () -> Unit
+) {
+    val colors = OutlinedTextFieldDefaults.colors(
+        focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+        unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+        // Disabled colors https://stackoverflow.com/a/76922565
+        disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+        disabledTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledBorderColor = MaterialTheme.colorScheme.outline,
+        disabledLeadingIconColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledTrailingIconColor = MaterialTheme.colorScheme.onSurface,
+        disabledLabelColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledPlaceholderColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledSupportingTextColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledPrefixColor = MaterialTheme.colorScheme.onSurfaceVariant,
+        disabledSuffixColor = MaterialTheme.colorScheme.onSurfaceVariant
+    )
+
+    if (date == null) {
+        TextField(
+            value = stringResource(R.string.no_due_date),
+            onValueChange = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onDatePickerClick() },
+            enabled = false,
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.calendar_month_24px),
+                    contentDescription = null
+                )
+            },
+            colors = colors
+        )
+    } else {
+        TextField(
+            value = date,
+            onValueChange = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onDatePickerClick() },
+            enabled = false,
+            label = { Text(stringResource(R.string.due_date)) },
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.calendar_month_24px),
+                    contentDescription = null
+                )
+            },
+            colors = colors,
+        )
     }
 }
--- app/src/main/java/com/example/quests/ui/task/AddTaskViewModel.kt
+++ app/src/main/java/com/example/quests/ui/task/AddTaskViewModel.kt
@@ -11,11 +11,13 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-// change this to have a isEntryValid: Boolean field
 data class AddTaskUiState(
     val title: String = "",
     val description: String = "",
     val userMessage: Int? = null,
+    val selectedDate: Long? = null,
+    val selectedHour: Int? = null,
+    val selectedMinute: Int? = null,
     val isEntryValid: Boolean = false,
     val isTaskSaved: Boolean = false
 )
@@ -45,6 +47,22 @@ class AddTaskViewModel @Inject constructor(
         }
         validateEntry()
     }
+
+    fun updateSelectedDate(newSelectedDate: Long?) {
+        _uiState.update {
+            it.copy(selectedDate = newSelectedDate)
+        }
+        validateEntry()
+    }
+
+    // give users option to choose no date and no time
+    // they can choose yes date, no time (the time will be assumed to be last ms of day)
+    // they cannot choose yes time, not date. if they choose any time, automatically
+    // choose tomorrow for them. if they try to set it to no date, set it to no time.
+    //
+    // take the current time, and the passed time. if the hour and minute of the passed
+    // time is greater than current time, then set due date to today. if hour and
+    // minute of passed time is less than current time, then set due date to tomorrow.
 
     private fun validateEntry() {
         _uiState.update {
