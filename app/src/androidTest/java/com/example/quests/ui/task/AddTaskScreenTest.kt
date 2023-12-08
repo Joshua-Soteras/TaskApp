a7a809ba5ede0a2fccc82d0d913252c5a9038de3
MarkCanseco252 <mcansec4@calstatela.edu>
2023-12-08 00:08:53 -0800

    Update AddTaskScreenTest.kt

--- app/src/androidTest/java/com/example/quests/ui/task/AddTaskScreenTest.kt
+++ app/src/androidTest/java/com/example/quests/ui/task/AddTaskScreenTest.kt
@@ -109,4 +109,25 @@ class AddTaskScreenTest {
         val tasks = repository.getAllTasksStream().first()
         tasks.size shouldBe 0
     }
+    @Test
+    fun uiInteraction_saveButtonBehavior() = runTest {
+        // GIVEN - an empty repository
+        repository.deleteAllTasks()
+
+        // WHEN - title and description are filled out
+        addTask(composeTestRule) {
+            assertSaveIsNotEnabled() // Save button should be initially disabled
+            title("Test Title")
+            description("Test Description")
+            assertSaveIsEnabled() // Save button should be enabled now
+        } save { }
+
+        composeTestRule.awaitIdle()
+
+        // THEN - the task should be saved in the repository
+        val tasks = repository.getAllTasksStream().first()
+        tasks.size shouldBe 1
+        tasks[0].title shouldBe "Test Title"
+        tasks[0].description shouldBe "Test Description"
+    }
 }
